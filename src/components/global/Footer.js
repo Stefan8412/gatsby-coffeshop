6d366344bf10e6b685c12d2de10c7127af39f9fd
Stefan Hancar <stefanhancar@MacBook-Air-uzivatela-Stefan.local>
2020-07-23 16:34:49 +0200

    update

--- src/components/global/Footer.js
+++ src/components/global/Footer.js
@@ -7,7 +7,7 @@ export default function Footer() {
                 <div className="row">
                     <div className="col-10 mx-auto col-md-6 text-yellow
                     text-center text-capitalize">
-                        <h3>all rights reserverd &copy;{new Date().getFullYear().toString()}</h3>
+                        <h3>all rights reserved &copy;{new Date().getFullYear().toString()}</h3>
                     </div>
 
                 </div>
